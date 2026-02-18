6b55b122edc8a5191c44b58362feb3e2b589a601
Jenil Saliya <JenilSaliya>
2026-02-18 16:55:20 +0530

    Fix MongoDB connection to use

--- config/db.js
+++ config/db.js
@@ -1,21 +1,35 @@
-import mongoose from 'mongoose'
-import config from './config.js'
+// import mongoose from 'mongoose'
+// import config from './config.js'
 
-// connect to database 
+// // connect to database 
+// const connectDB = async () => {
+
+//     try {
+        
+//         const con = await mongoose.connect(config.db_URI)
+
+//     } 
+//     catch (err) {
+
+//         console.log(`Database connect error: ${err.message}`)
+//         process.exit(1)
+        
+//     }
+// }
+
+// connectDB()
+// export default connectDB
+
+import mongoose from 'mongoose';
+
 const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.Mongo_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("Database connect error:", err.message);
+    process.exit(1);
+  }
+};
 
-    try {
-        
-        const con = await mongoose.connect(config.db_URI)
-
-    } 
-    catch (err) {
-
-        console.log(`Database connect error: ${err.message}`)
-        process.exit(1)
-        
-    }
-}
-
-connectDB()
-export default connectDB
+export default connectDB;
