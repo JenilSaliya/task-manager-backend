bf545e0d87458da417ab9e90f87898cfbedd0c2d
Jenil Saliya <JenilSaliya>
2026-02-18 16:36:39 +0530

    Fix MongoDB Connection URI

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ dotenv.config()
 const config = {
 
     host_URI: process.env.Host_URI,
-    db_URI: process.env.Mongo_URI+"TMS",
+    db_URI: process.env.Mongo_URI,
     port:process.env.port,
     React_URI:process.env.React_URI
     
